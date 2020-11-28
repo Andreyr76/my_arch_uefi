7b87974d3ba88d725f55dada9ab77074a8fc6fb7
Andreyr76 <rabotbiz@gmail.com>
2020-11-28 21:53:05 +0300

    3

--- my_arch_uefi3.sh
+++ my_arch_uefi3.sh
@@ -7,104 +7,61 @@ sudo pacman -Syu
 sudo pacman -S wget --noconfirm
 wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
 
-echo 'Создаем нужные директории'
-sudo pacman -S xdg-user-dirs --noconfirm
-xdg-user-dirs-update
+#echo 'Создаем нужные директории'
+#sudo pacman -S xdg-user-dirs --noconfirm
+#xdg-user-dirs-update
+
+echo 'Удаляем лишнее'
+sudo pacman -Rs xfburn orage parole ristretto
 
 echo 'Установка базовых программ и пакетов'
-sudo pacman -S reflector firefox firefox-i18n-ru ufw f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol --noconfirm
+sudo pacman -S reflector firefox firefox-i18n-ru ufw f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol youtube-dl peek --noconfirm
 
-echo 'Установить рекомендумые программы?'
-read -p "1 - Да, 0 - Нет: " prog_set
-if [[ $prog_set == 1 ]]; then
-  #Можно заменить на pacman -Qqm > ~/.pacmanlist.txt
-  sudo pacman -S recoll chromium flameshot obs-studio veracrypt vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru kdenlive neofetch qbittorrent galculator telegram-desktop --noconfirm
-  yay -Syy
-  yay -S xflux sublime-text-dev hunspell-ru pamac-aur-git megasync-nopdfium trello xorg-xkill ttf-symbola ttf-clear-sans --noconfirm
-elif [[ $prog_set == 0 ]]; then
-  echo 'Установка программ пропущена.'
-fi
+sudo pacman -S chromium flameshot veracrypt vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru neofetch qbittorrent screenfetch plank telegram-desktop gparted evince keepassxc atom nano vim neovim featherpad fbreader audacious git ktouch klavaro clamtk mc htop tmux unrar zip ark gnome-disk-utility gedit uget smplayer smtube pycharm-community-edition --noconfirm
 
-echo 'Скачать и установить конфиг и темы для XFCE?'
-read -p "1 - Да, 0 - Нет: " xfce_set
-if [[ $xfce_set == 1 ]]; then
-  echo 'Качаем и устанавливаем настройки Xfce'
-  # Чтобы сделать копию ваших настоек перейдите в домашнюю директорию ~/username 
-  # открйте в этой категории терминал и выполните команду ниже
-  # Предварительно можно очистить конфиг от всего лишнего
-  # tar -czf config.tar.gz .config
-  # Выгрузите архив в интернет и скорректируйте ссылку на свою.
-  wget https://github.com/ordanax/arch/raw/master/attach/config.tar.gz
-  sudo rm -rf ~/.config/xfce4/*
-  sudo tar -xzf config.tar.gz -C ~/
-  echo 'Удаление тем по умолчанию'
-  sudo rm -rf /usr/share/themes/*
-  echo 'Установка тем'
-  yay -S x-arc-shadow papirus-maia-icon-theme-git breeze-default-cursor-theme --noconfirm
-  sudo pacman -S capitaine-cursors --noconfirm
-  
-  echo 'Ставим лого ArchLinux в меню'
-  wget git.io/arch_logo.png
-  sudo mv -f ~/downloads/arch_logo.png /usr/share/pixmaps/arch_logo.png
-  
-  echo 'Удаляем лишнее из xfce4'
-  sudo pacman -Rs xfburn orage parole mousepad xfce4-appfinder xfce4-clipman-plugin xfce4-timer-plugin xfce4-time-out-plugin xfce4-artwork xfce4-taskmanager xfce4-smartbookmark-plugin xfce4-sensors-plugin xfce4-screenshooter xfce4-notes-plugin xfce4-netload-plugin xfce4-mpc-plugin xfce4-mount-plugin xfce4-mailwatch-plugin xfce4-genmon-plugin xfce4-fsguard-plugin xfce4-eyes-plugin xfce4-diskperf-plugin xfce4-dict xfce4-cpugraph-plugin xfce4-cpufreq-plugin
+gpg --keyserver keys.gnupg.net --recv-keys FC918B335044912E 
 
-  echo 'Ставим обои на рабочий стол'
-  wget git.io/bg.jpg
-  sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартные обои
-  sudo mv -f ~/downloads/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
-elif [[ $xfce_set == 0 ]]; then
-  echo 'Установка конфигов XFCE пропущена.'
-fi 
+yay -Syy
+yay -S xflux hunspell-ru pamac-aur-git conky-lua timeshift tusk vscodium-bin figma-linux xorg-xkill teamviewer etcher-bin garuda-settings-manager-git dropbox ttf-symbola ttf-clear-sans --noconfirm
 
-echo "Ставим i3 с моими настройками?"
-read -p "1 - Да, 2 - Нет: " vm_setting
-if [[ $vm_setting == 1 ]]; then
-    pacman -S pacman -S i3-wm polybar dmenu pcmanfm ttf-font-awesome feh gvfs udiskie xorg-xbacklight ristretto tumbler compton jq --noconfirm
-    yay -S polybar ttf-weather-icons ttf-clear-sans
-    wget https://github.com/ordanax/arch/raw/master/attach/config_i3wm.tar.gz
-    sudo rm -rf ~/.config/i3/*
-    sudo rm -rf ~/.config/polybar/*
-    sudo tar -xzf config_i3wm.tar.gz -C ~/
-elif [[ $vm_setting == 2 ]]; then
-  echo 'Пропускаем.'
-fi
+echo 'Качаем и устанавливаем настройки Xfce'
+# Чтобы сделать копию ваших настоек перейдите в домашнюю директорию ~/username
+# открйте в этой категории терминал и выполните команду ниже
+# Предварительно можно очистить конфиг от всего лишнего
+# tar -czf config.tar.gz .config
+# Выгрузите архив в интернет и скорректируйте ссылку на свою.
+wget https://github.com/Andreyr76/my_arch_uefi/raw/main/attach/config.tar.gz
+sudo rm -rf ~/.config/xfce4/*
+sudo tar -xzf config.tar.gz -C ~/
 
-echo 'Установить conky?'
-read -p "1 - Да, 0 - Нет: " conky_set
-if [[ $conky_set == 1 ]]; then
-  sudo pacman -S conky conky-manager --noconfirm
-  wget git.io/conky.tar.gz
-  tar -xzf conky.tar.gz -C ~/
-elif [[ $conky_set == 0 ]]; then
-  echo 'Установка conky пропущена.'
-fi
+#echo 'Удаление тем по умолчанию'
+#sudo rm -rf /usr/share/themes/*
 
-echo 'Делаем авто вход без DE?'
-read -p "1 - Да, 0 - Нет: " node_set
-if [[ $node_set == 1 ]]; then
-sudo systemctl disable lxdm
-sudo pacman -R lxdm
-sudo pacman -S xorg-xinit --noconfirm
-cp /etc/X11/xinit/xserverrc ~/.xserverrc
-wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.xinitrc
-sudo mv -f .xinitrc ~/.xinitrc
-wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.bashrc
-rm ~/.bashrc
-sudo mv -f .bashrc ~/.bashrc
-wget https://raw.githubusercontent.com/ordanax/arch/master/attach/grub
-sudo mv -f grub /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-read -p "Введите имя пользователя: " username
-sudo echo -e '[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin' "$username" '--noclear %I $TERM' > ~/downloads/override.conf
-sudo mkdir /etc/systemd/system/getty@tty1.service.d/
-sudo mv -f ~/downloads/override.conf /etc/systemd/system/getty@tty1.service.d/override.conf
-elif [[ $node_set == 0 ]]; then
-  echo 'Пропускаем.'
-fi
+echo 'Установка тем'
+wget https://github.com/Andreyr76/my_arch_uefi/raw/main/attach/themes.tar.gz
+sudo tar -xzf themes.tar.gz -C ~/
 
-# Подключаем zRam
+wget https://github.com/Andreyr76/my_arch_uefi/raw/main/attach/icons.tar.gz
+sudo tar -xzf icons.tar.gz -C ~/
+
+yay -S x-arc-shadow papirus-maia-icon-theme-git breeze-default-cursor-theme breeze-obsidian-cursor-theme xcursor-breeze-serie-obsidian --noconfirm
+
+sudo pacman -S capitaine-cursors --noconfirm
+
+echo 'Ставим лого ArchLinux в меню'
+wget https://github.com/Andreyr76/my_arch_uefi/raw/main/attach/archlinux_logo.png
+sudo mv -f ~/downloads/arch_logo.png /usr/share/pixmaps/arch_logo.png
+
+echo 'Ставим обои на рабочий стол'
+wget https://github.com/Andreyr76/my_arch_uefi/raw/main/attach/bg.jpg
+sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартные обои
+sudo mv -f ~/downloads/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
+
+echo 'Conky & bashrc'
+wget https://github.com/Andreyr76/my_arch_uefi/raw/main/attach/bash.tar.gz
+tar -xzf bash.tar.gz -C ~/
+
+echo 'Подключаем zRam'
 yay -S zramswap --noconfirm
 sudo systemctl enable zramswap.service
 
@@ -114,7 +71,7 @@ sudo ufw enable
 echo 'Добавляем в автозагрузку:'
 sudo systemctl enable ufw
 
-# Очистка
+echo 'Очистка''
 rm -rf ~/downloads/
 
 echo 'Установка завершена!'
